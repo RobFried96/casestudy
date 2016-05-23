ed9fe38ba0b69afca478a22872bbb95d6b71a24a
RobFried96 <robinfriedrich@live.de>
2016-05-23 11:20:16 +0200

    Update DB BuyVision.sql

--- DB BuyVision.sql
+++ DB BuyVision.sql
@@ -14,6 +14,7 @@ CREATE TABLE `Produkt` (
 `Verpackungseinheit` VARCHAR(20),
 `Preis_Zeit` TIMESTAMP,
 FOREIGN KEY (Preis_Zeit) REFERENCES Preis(Zeit)
+ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `Substitut` (
@@ -23,6 +24,7 @@ CREATE TABLE `Substitut` (
 `Verpackungseinheit` VARCHAR(20),
 `Preis_Zeit` TIMESTAMP,
 FOREIGN KEY (Preis_Zeit) REFERENCES Preis(Zeit)
+ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `Kette` (
@@ -38,6 +40,7 @@ CREATE TABLE `Supermarkt` (
 `Adresse_Land` VARCHAR(20) DEFAULT 'Deutschland',
 `Kette_Name` VARCHAR(20) NOT NULL,
 FOREIGN KEY (Kette_Name) REFERENCES Kette(Name)
+ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
@@ -72,22 +75,27 @@ CREATE TABLE `Statistics` (
 `Referer` VARCHAR(2000),
 `User_User_ID` INT,
 FOREIGN KEY (User_User_ID) REFERENCES User(User_ID)
+ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `User_has_Einkaufszettel` (
 `User_User_ID` INT,
 `Einkaufszettel_User_ID` INT,
 PRIMARY KEY(User_User_ID, Einkaufszettel_User_ID),
-FOREIGN KEY (User_User_ID) REFERENCES User(User_ID),
+FOREIGN KEY (User_User_ID) REFERENCES User(User_ID)
+ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY (Einkaufszettel_User_ID) REFERENCES Einkaufszettel(User_ID)
+ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `Supermarkt_has_Produkt` (
 `Produkt_Produktnummer` INT,
 `Supermarkt_Supermarkt_ID` INT,
 PRIMARY KEY(Produkt_Produktnummer, Supermarkt_Supermarkt_ID),
-FOREIGN KEY (Produkt_Produktnummer) REFERENCES Produkt(Produktnummer),
+FOREIGN KEY (Produkt_Produktnummer) REFERENCES Produkt(Produktnummer)
+ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY (Supermarkt_Supermarkt_ID) REFERENCES Supermarkt(Supermarkt_ID)
+ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `Einkaufszettel_has_Produkt` (
@@ -98,14 +106,18 @@ CREATE TABLE `Einkaufszettel_has_Produkt` (
 `Einheit` VARCHAR(10),
 `Supermarkt_ID` INT,
 PRIMARY KEY(Produkt_Produktnummer, Einkaufszettel_User_ID),
-FOREIGN KEY (Produkt_Produktnummer) REFERENCES Produkt(Produktnummer),
+FOREIGN KEY (Produkt_Produktnummer) REFERENCES Produkt(Produktnummer)
+ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY (Einkaufszettel_User_ID) REFERENCES Einkaufszettel(User_ID)
+ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `Produkt_has_Substitut` (
 `Produkt_Produktnummer` INT,
 `Substitut_Produktnummer` INT,
 PRIMARY KEY (Produkt_Produktnummer, Substitut_Produktnummer),
-FOREIGN KEY (Produkt_Produktnummer) REFERENCES Produkt(Produktnummer),
+FOREIGN KEY (Produkt_Produktnummer) REFERENCES Produkt(Produktnummer)
+ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY (Substitut_Produktnummer) REFERENCES Substitut(Produktnummer)
+ON DELETE CASCADE ON UPDATE CASCADE
 );
